dc552467d5f829311248c4fa6389f2ac99d2f9d4
Piotr Waszak <57100427+wiotrek@users.noreply.github.com>
2022-08-28 20:39:26 +0200

    Stage0#2 (#6)
    * #2 added shared module and auth module
    
    * close #2
    
    Co-authored-by: wiotrek <wiotrek@thinkpad.thinkpad>

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { AuthComponent } from 'src/app/auth/auth.component';
+
+@NgModule({
+  declarations: [AuthComponent]
+})
+export class AuthModule {}
