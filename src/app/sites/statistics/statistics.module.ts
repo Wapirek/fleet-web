846d2c6e6fc4102be508c3c58afa9280f5614f17
wiotrek <wiotrek@thinkpad.thinkpad>
2022-08-28 20:59:37 +0200

    #3 podlaczone sciezki dla komponentow istniejacych w modulach ladowanych leniwie

--- src/app/sites/statistics/statistics.module.ts
+++ src/app/sites/statistics/statistics.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { StatisticsComponent } from 'src/app/sites/statistics/statistics.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     StatisticsComponent
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: '', component: StatisticsComponent }
+    ])
   ]
 })
 export class StatisticsModule {}
--- src/app/sites/transactions/transactions.module.ts
+++ src/app/sites/transactions/transactions.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TransactionsComponent } from 'src/app/sites/transactions/transactions.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     TransactionsComponent
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: '', component: TransactionsComponent }
+    ])
   ]
 })
 export class TransactionsModule {}
