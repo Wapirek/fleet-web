f4eee48c265f324b24bb2a1aabc0bc6a1b72824e
Piotr Waszak <57100427+wiotrek@users.noreply.github.com>
2022-09-04 18:21:16 +0200

    Stage0#5 (#9)
    * #5 zaimplementowany angular material
    
    * #5 zaimplementowane fontawesome 6
    
    Co-authored-by: wiotrek <wiotrek@thinkpad.thinkpad>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SitesComponent } from 'src/app/sites/sites.component';
 import { DashboardComponent } from './sites/dashboard/dashboard.component';
 import { NavbarComponent } from './sites/navbar/navbar.component';
 import { SettingsComponent } from './sites/settings/settings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { SettingsComponent } from './sites/settings/settings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   bootstrap: [AppComponent]
 })
