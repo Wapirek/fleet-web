42f09d711f0780ea528a031afca599cd8e13db2c
Piotr Waszak <57100427+wiotrek@users.noreply.github.com>
2022-09-05 21:03:32 +0200

    Stage1#10 (#14)
    * #10 dodany background dla calego projektu, oraz rozpoczete prace przy pasku nawigacyjnym
    
    * #10 dodana nazwa uzytkownika i ikona po prawej stronie
    
    * #10 stylizacja przycisku z nazwa uzytkownika
    
    * #10 poprawa rwd, oraz dodanie dropdowna
    
    * #10 poprawa rwd, oraz dodanie dropdowna - brakujace pliki
    
    Co-authored-by: wiotrek <wiotrek@thinkpad.thinkpad>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: []
+  declarations: [],
+  imports: [
+    MatMenuModule,
+    FontAwesomeModule
+  ],
+  exports: [
+    MatMenuModule,
+    FontAwesomeModule
+  ]
 })
 export class SharedModule {}
--- src/app/sites/sites.component.ts
+++ src/app/sites/sites.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sites',
   template: `
-    <div class="center">
-      <app-navbar></app-navbar>
-      <router-outlet></router-outlet>
-    </div>
+    <app-navbar></app-navbar>
+    <div class="center"><router-outlet></router-outlet></div>
   `,
   styleUrls: ['./sites.component.scss']
 })
