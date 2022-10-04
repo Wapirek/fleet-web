d57aa6b91ad56b8538509bed2a30a088f38c21b9
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-04 18:05:25 +0200

    #24 zaktualizowany navbar dla linkow

---
+++ src/app/sites/navbar/_helpers/structure-builder.helper.ts
@@ -0,0 +1,27 @@
+interface StructureBuilderModel {
+  links: {
+    name: string;
+    url: string;
+  }[];
+}
+
+export const StructureBuilderHelper: StructureBuilderModel = {
+  links: [
+    {
+      name: 'Dashboard',
+      url: ''
+    },
+    {
+      name: 'Transakcje',
+      url: 'transakcje'
+    },
+    {
+      name: 'Statystyki',
+      url: 'statystyki'
+    },
+    {
+      name: 'Ustawienia',
+      url: 'ustawienia'
+    }
+  ]
+};
