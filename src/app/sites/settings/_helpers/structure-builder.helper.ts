7276843a9b794eba7c0798b658d20f03eeaf1eec
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-06 22:02:20 +0200

    #25 wstepny layout ustawien

---
+++ src/app/sites/settings/_helpers/structure-builder.helper.ts
@@ -0,0 +1,12 @@
+import { HeaderModel } from 'src/app/shared/models/structure-html/header.model';
+
+interface StructureBuilderModel {
+  header: HeaderModel;
+}
+
+export const StructureBuilderHelper: StructureBuilderModel = {
+  header: {
+    icon: 'tune',
+    title: 'Ustawienia'
+  }
+}
--- src/app/sites/settings/settings.component.ts
+++ src/app/sites/settings/settings.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { StructureBuilderHelper } from 'src/app/sites/settings/_helpers/structure-builder.helper';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  skeleton = StructureBuilderHelper;
 }
