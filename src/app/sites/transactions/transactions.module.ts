74c054238eb20c2ef79b85057cce179e956d0156
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-06 21:00:29 +0200

    #24 ukonczenie wstepnego layoutu transakcji

--- src/app/sites/transactions/transactions.module.ts
+++ src/app/sites/transactions/transactions.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { TransactionsComponent } from 'src/app/sites/transactions/transactions.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     TransactionsComponent
   ],
   imports: [
-    RouterModule.forChild([ { path: '', component: TransactionsComponent } ]),
-    SharedModule
+    RouterModule.forChild([{path: '', component: TransactionsComponent}]),
+    SharedModule,
+    MatTableModule
   ]
 })
 export class TransactionsModule {}
