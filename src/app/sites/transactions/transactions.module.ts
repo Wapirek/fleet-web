0431895157428acdf73142f140cefe8c3da69eb1
wiotrek <piotrwaszak97@gmail.com>
2022-10-09 22:02:40 +0200

    #36 dodanie paginacji w tabeli

--- src/app/sites/transactions/transactions.module.ts
+++ src/app/sites/transactions/transactions.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatTableModule } from '@angular/material/table';
 import { DisplayColsNamePipe } from 'src/app/sites/transactions/_pipes/display-cols-name/display-cols-name.pipe';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { DisplayColsNamePipe } from 'src/app/sites/transactions/_pipes/display-c
   imports: [
     RouterModule.forChild([{path: '', component: TransactionsComponent}]),
     SharedModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ]
 })
 export class TransactionsModule {}
