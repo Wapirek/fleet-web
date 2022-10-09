ea02ce6f06da19c0df29d1ecb84dd4a8d3d381ae
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-09 16:49:04 +0200

    #36 dodanie listy kolumn

--- src/app/sites/transactions/transactions.component.ts
+++ src/app/sites/transactions/transactions.component.ts
@@ -1,25 +1,6 @@
 import { Component } from '@angular/core';
 import { CodeNameWidget, StructureBuilderHelper } from 'src/app/sites/transactions/_helpers/structure-builder.helper';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+import { DisplayedColumnsArray, DisplayedColumnsModel } from 'src/app/sites/transactions/_arrays/displayed-columns.array';
 
 @Component({
   selector: 'app-transactions',
@@ -28,12 +9,13 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class TransactionsComponent {
 
+  // skeleton to create template
   skeleton = StructureBuilderHelper;
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  // columns
+  displayedColumns: DisplayedColumnsModel[] = DisplayedColumnsArray;
 
-  constructor() {}
+  dataSource: any;
 
   addTransaction(): void {}
 
