5161f05781a11f01cc2f954abb87d44dffd11c7c
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-09 15:54:24 +0200

    close #35

--- src/app/sites/statistics/statistics.module.ts
+++ src/app/sites/statistics/statistics.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { StatisticsComponent } from 'src/app/sites/statistics/statistics.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
     StatisticsComponent
   ],
   imports: [
+    SharedModule,
     RouterModule.forChild([
       { path: '', component: StatisticsComponent }
     ])
