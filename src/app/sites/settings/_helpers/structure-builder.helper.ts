e4ef6b082f80200554812a5a02adb90fc468a295
wiotrek <wiotrek@thinkpad.thinkpad>
2022-10-20 19:29:28 +0200

    #38 dodane przyciski w ustawieniach

--- src/app/sites/settings/_helpers/structure-builder.helper.ts
+++ src/app/sites/settings/_helpers/structure-builder.helper.ts
@@ -1,12 +1,27 @@
 import { HeaderModel } from 'src/app/shared/models/structure-html/header.model';
+import { faCircleUser, IconDefinition } from '@fortawesome/free-regular-svg-icons';
 
 interface StructureBuilderModel {
   header: HeaderModel;
+  leftSide: {
+    user: {
+      logo: IconDefinition;
+      name: string;
+    };
+    buttons: string[]
+  }
 }
 
 export const StructureBuilderHelper: StructureBuilderModel = {
   header: {
     icon: 'tune',
     title: 'Ustawienia'
+  },
+  leftSide: {
+    user: {
+      logo: faCircleUser,
+      name: ''
+    },
+    buttons: ['Zmień hasło', 'Zmień nazwę', 'Usuń konto', 'Wyloguj się']
   }
 }
