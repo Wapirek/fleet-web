94365145e141d88f9a08e95b9513a0f38fc282cc
wiotrek <wiotrek@thinkpad.thinkpad>
2022-11-01 16:53:55 +0100

    #39 wstepny mock popupa

--- src/app/shared/components/modals/transaction-modal/transaction-modal.component.ts
+++ src/app/shared/components/modals/transaction-modal/transaction-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { StructureBuilderHelper } from 'src/app/shared/components/modals/transaction-modal/_helpers/structure-builder.helper';
 
 @Component({
   selector: 'shared-transaction-modal',
@@ -9,4 +10,6 @@ export class TransactionModalComponent {
 
   // active when user click on cross
   @Output() closeModal = new EventEmitter<void>();
+
+  formStructure = StructureBuilderHelper();
 }
