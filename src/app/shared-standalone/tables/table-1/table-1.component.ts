99f3ef1716d188c60a1f068d7a92925263eb8e6d
wiotrek <wiotrek@thinkpad.thinkpad>
2022-12-05 21:16:34 +0100

    #68 problem z przekazaniem sort i paginacji do dziecka

--- src/app/shared-standalone/tables/table-1/table-1.component.ts
+++ src/app/shared-standalone/tables/table-1/table-1.component.ts
@@ -1,16 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { DisplayedColumnsModel } from 'src/app/sites/transactions/_arrays/displayed-columns.array';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 @Component({
   standalone: true,
   selector: 'shared-standalone-table-1',
   templateUrl: './table-1.component.html',
   styleUrls: ['./table-1.component.scss'],
-  imports: [SharedModule, MatTableModule]
+  imports: [
+    SharedModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ]
 })
-export class Table1Component<T> {
+export class Table1Component<T> implements OnInit {
 
   // lista kolumn przypisana do tabeli
   @Input() displayedColumns: DisplayedColumnsModel[] = [];
@@ -20,4 +27,13 @@ export class Table1Component<T> {
 
   // pole wyszukiwania
   @Output() searcher = new EventEmitter<string>();
+
+  // przypisanie do zmiennych sortowania i pagniacji z html
+  @Input() sort!: MatSort;
+  @Input() paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 }
--- src/app/sites/settings/profits/profits.component.ts
+++ src/app/sites/settings/profits/profits.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { SettingsService } from 'src/app/sites/settings/_service/settings.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { DisplayedColumnsArray } from 'src/app/sites/settings/profits/_arrays/displayed-columns.array';
 import { ProfitModel } from 'src/app/shared/models/models/settings/profit.model';
 import { first, Subject, Subscription } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   templateUrl: './profits.component.html',
   styleUrls: ['./profits.component.scss']
 })
 export class ProfitsComponent implements OnInit, OnDestroy {
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  // getting paginator emitter from material component
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   // lista kolumn przypisana do tabeli
   displayedColumns = DisplayedColumnsArray;
@@ -25,10 +33,11 @@ export class ProfitsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
 
-    this.searcher$.subscribe(s => console.log(s));
+    this.dataSource.connect().subscribe(s => console.log(s))
 
-    this.subscription = this.settingsService.getCashFlowList()
-      .pipe(first())
+    this.sort?.sortChange.subscribe(s => console.log(s))
+
+    this.settingsService.getCashFlowList()
       .subscribe(r => {
         console.log(r);
         this.dataSource.data = r;
