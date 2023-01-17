a559a6677fbf0c9174a4d00897c4b7ff52cc06ee
Piotr Waszak <57100427+wiotrek@users.noreply.github.com>
2023-01-17 19:51:15 +0100

    Stage5 92 (#93)
    * #92 dodanie wlasciwych formbuilderow w modalach
    
    * #92 poprawiony blad z broadcrumbem w ustawieniach - przychodach
    
    * #92 poprawiony <title>

--- src/app/shared-standalone/modals/goal-modal/goal-modal.component.ts
+++ src/app/shared-standalone/modals/goal-modal/goal-modal.component.ts
@@ -1,12 +1,15 @@
 import { Component, EventEmitter } from '@angular/core';
 import { StructureBuilderHelper } from 'src/app/shared-standalone/modals/goal-modal/_helpers/structure-builder.helper';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilderHelper } from 'src/app/shared-standalone/modals/goal-modal/_helpers/form-builder.helper';
 
 @Component({
   standalone: true,
   selector: 'goal-modal',
   template: `
     <shared-modal-style-1
+      [cmpForm]="cmpForm"
       [formInitArray]="structureBuilder"
       [title]="title"
       (closeModal)="closeModal.emit()"
@@ -14,6 +17,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [SharedModule]
 })
 export class GoalModalComponent {
+
+  // formularz dla komponentu
+  cmpForm: FormGroup = FormBuilderHelper(this.formBuilder);
 
   // tutuł komponentu
   title = 'Dodajesz nowy cel';
@@ -23,4 +29,6 @@ export class GoalModalComponent {
 
   // przycisk zamykajacy modal
   closeModal = new EventEmitter<void>();
+
+  constructor(private formBuilder: FormBuilder) {}
 }
